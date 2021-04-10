b1c262192492a2e9a556c564db821e44b4a914e2
Dillon O'Connor <dilong818@gmail.com>
2021-04-10 14:20:16 -0400

    init commit

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,17 @@
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+
+import Rails from "@rails/ujs"
+import "@hotwired/turbo-rails"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
+
+Rails.start()
+ActiveStorage.start()
+
+window.addEventListener("turbo:before-fetch-response", () => {
+  const message = document.querySelector("#message");
+  message.style.opacity = "0.7";
+});
