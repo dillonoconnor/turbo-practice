e6d58572667f6f51a7ef9583a62fa94419551eb3
Dillon O'Connor <dilong818@gmail.com>
2021-04-12 21:45:23 -0400

    fix disappearing message input form with correct controller redirect

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -26,10 +26,10 @@ class MessagesController < ApplicationController
 
     respond_to do |format|
       if @message.save
-        format.turbo_stream
-        format.html { redirect_to @message, notice: "Message was successfully created." }
+        format.html { redirect_to messages_url, notice: "Message was successfully created." }
         format.json { render :show, status: :created, location: @message }
       else
+        format.turbo_stream { render turbo_stream: turbo_stream.replace(@message, partial: "messages/form", locals: { message: @message }) }
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @message.errors, status: :unprocessable_entity }
       end
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,6 @@
 class Message < ApplicationRecord
-
   after_create_commit { broadcast_append_later_to "thing", target: "message-container", partial: "messages/message" }
+
+  validates :content, presence: true
 
 end
