f38abf5b2366d76e0f357c71edcaf7e2beae7c18
Dillon O'Connor <dilong818@gmail.com>
2021-04-13 23:00:13 -0400

    broadcast replace and destroy on model update

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,5 +1,7 @@
 class Message < ApplicationRecord
   after_create_commit { broadcast_append_later_to "thing", target: "message-container", partial: "messages/message" }
+  after_update_commit { broadcast_replace_later_to "thing" }
+  after_destroy_commit { broadcast_remove_to "thing" }
 
   validates :content, presence: true
 
